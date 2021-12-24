97687cf76f08be4bbcc0d3754b42ce2f368ad234
yedd77 <52603129+yedd77@users.noreply.github.com>
2021-12-24 17:31:04 +0800

    V3

--- src/StockInventory/Product_A.java
+++ src/StockInventory/Product_A.java
@@ -4,14 +4,14 @@ public class Product_A {
     private String ProductName; //var for name of the product
     private float ProductPrice; //var for price of the product
     private int ProductQuantity; //var for the quantity of units in stock
-    private float ProductID;    //var for the ID number of the product
+    private String ProductID;    //var for the ID number of the product
     private String ProductBrand; //var for the brand of the product
 
     //default constructor
     Product_A(){}
 
     //overloading constructor
-    Product_A(String ProductName, float ProductPrice, int ProductQuantity, float ProductID, String ProductBrand ){
+    Product_A(String ProductName, float ProductPrice, int ProductQuantity, String ProductID, String ProductBrand){
         this.ProductName = ProductName;
         this.ProductPrice = ProductPrice;
         this.ProductQuantity = ProductQuantity;
@@ -23,45 +23,54 @@ public class Product_A {
     public String getProductName() {
         return ProductName;
     }
+
     //set method for var ProductName
     public void setProductName(String productName) {
         ProductName = productName;
     }
+
     //get method for var ProductPrice
     public float getProductPrice() {
         return ProductPrice;
     }
+
     //set method for var ProductPrice
     public void setProductPrice(float productPrice) {
         ProductPrice = productPrice;
     }
+
     //get method for var ProductQuantity
     public int getProductQuantity() {
         return ProductQuantity;
     }
+
     //set method for var ProductQuantity
     public void setProductQuantity(int productQuantity) {
         ProductQuantity = productQuantity;
     }
+
     //get method for var ProductID
-    public float getProductID() {
+    public String getProductID() {
         return ProductID;
     }
+
     //set method for var ProductID
-    public void setProductID(float productID) {
+    public void setProductID(String productID) {
         ProductID = productID;
     }
+
     //get method for var ProductBrand
     public String getProductBrand() {
         return ProductBrand;
     }
+
     //set method for var ProductBrand
     public void setProductBrand(String productBrand) {
         ProductBrand = productBrand;
     }
-    //Method to calculate inventory value
-    public float Inventory_Value(){
-        return ProductPrice*ProductQuantity ;
 
+    //Method to calculate inventory value
+    public float Inventory_Value(float ProdPrice, float ProdQuantity){
+        return ProdPrice*ProdQuantity ;
 }
 }   
--- src/StockInventory/Product_A_Tester.java
+++ src/StockInventory/Product_A_Tester.java
@@ -1,16 +1,60 @@
 package StockInventory;
+
 import java.io.*;
 
 public class Product_A_Tester {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        //variable declaration
         String ProdName;
-        float ProdPrice;
-        int ProdQuantity;
+        float ProdPrice = 0;
+        int ProdQuantity = 0;
         String ProdID;
         String ProdBrand;
 
-        
+
+        //user input
+        System.out.print("Enter the name of the product : ");
+        ProdName = br.readLine();
+
+        try {
+            System.out.print("Enter the price of the product : ");
+            ProdPrice = Float.parseFloat(br.readLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Exception Occured! Invalid Data");
+        }
+
+        try {
+            System.out.print("Enter the quantity of product unit it stock : ");
+            ProdQuantity = Integer.parseInt(br.readLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Exception Occured! Invalid Data");
+        }
+
+        System.out.print("Enter the product ID : ");
+        ProdID = br.readLine();
+
+        System.out.print("Enter the brand of the product : ");
+        ProdBrand = br.readLine();
+        br.close();
+
+        //create object for Product A
+        Product_A prod = new Product_A();
+
+        //update variable value
+        prod.setProductName(ProdName);
+        prod.setProductPrice(ProdPrice);
+        prod.setProductQuantity(ProdQuantity);
+        prod.setProductID(ProdID);
+        prod.setProductBrand(ProdBrand);
+
+        // Output
+        System.out.println("Product Name : " +prod.getProductName() );
+        System.out.println("Product Price : " +prod.getProductPrice() );
+        System.out.println("Quantity of product units in stock : " +prod.getProductQuantity());
+        System.out.println("Product ID Number : " +prod.getProductID() );
+        System.out.println("Product Brand : " +prod.getProductBrand());
+        System.out.println("Inventory Value :" + prod.Inventory_Value(ProdPrice, ProdQuantity));
     }
 }
